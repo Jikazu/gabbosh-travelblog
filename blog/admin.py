ebf12dcd2e7add6d1299c02996d2dd9f3e455671
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-11-27 04:58:13 +0000

    added management for comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -11,4 +11,8 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     summernote_fields=('content')
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'approved', 'created_on')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
