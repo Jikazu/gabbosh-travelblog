29d23ae7238896ff54f883d22548d91a8444727c
Gabriella Köhler <gabriella.kohler91@gmail.com>
2023-12-13 10:28:45 +0000

    deployment commit

--- gabbosh/settings.py
+++ gabbosh/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['gabbosh-travelblog.herokuapp.com', 'localhost', 'gabbosh-travelblog-6ba49b37d1ec.herokuapp.com', '8000-jikazu-gabboshtravelblo-3ap0jve49dh.ws-eu106.gitpod.io']
 
